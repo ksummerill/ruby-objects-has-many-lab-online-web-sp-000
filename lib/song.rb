84d1273ce546fb02d149985cf7dad96a792c5ad9
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-08 14:51:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,11 +16,7 @@ class Song
 
   # returns nil if the song does not have an artist
   def artist_name
-    # self.artist.name if self.artist
-    if self.artist.name
-    else self.artist
-      return nil
-    end
+    self.artist.name if self.artist
   end
 
 end
