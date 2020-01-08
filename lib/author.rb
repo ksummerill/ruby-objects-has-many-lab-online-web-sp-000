3a9217207d2cc0ad0d620873581eb223edf11d72
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-08 15:05:55 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,7 +14,13 @@ class Author
   # by telling the post that it belongs to that author
   def add_post(post)
     post.author = self
+  end
 
+  # takes in an argument of a post title,
+  # creates a new post with it and associates the post and author
+  def add_post_by_title(post)
+    Post.new(post)
+    post.author = self
   end
 
 end
