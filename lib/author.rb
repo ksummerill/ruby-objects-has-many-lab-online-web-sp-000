795873efee097dbfe37b1f143dfc43d900ffdef4
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-08 15:03:16 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,4 +10,11 @@ class Author
     Post.all
   end
 
+  # takes in an argument of a post and associates that post with the author
+  # by telling the post that it belongs to that author
+  def add_post(post)
+    author.post = post
+    
+  end
+
 end
